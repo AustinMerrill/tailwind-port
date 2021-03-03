b1f1c377e4f3da7f1f9ab7c8eda18cddae99e64f
Austin Merrill <austinmerrill@Austins-MBP.lan>
2021-03-03 16:35:54 -0600

    Added id to index page

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const nav = document.getElementById('nav');
 const toggleOpen = document.getElementById('toggleOpen');
 const toggleClose = document.getElementById('toggleClose');
+const h1Text =document.getElementById('h1Text');
 
 toggleOpen.addEventListener('click', ()=> {
     toggleOpen.classList.toggle('hidden')
     toggleClose.classList.toggle('hidden')
     nav.classList.toggle('hidden')
+    h1Text.classList.toggle('hidden')
+
 })
 
 toggleClose.addEventListener('click', () => {
     toggleOpen.classList.toggle('hidden')
     toggleClose.classList.toggle('hidden')
     nav.classList.toggle('hidden')
+    h1Text.classList.toggle('hidden')
 })
